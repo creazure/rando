9c38aeb3c00b3709ce2fb60d29fc69ec54580be0
Anis <ajmiba@yahoo.fr>
2017-11-22 15:27:43 +0100

    hike component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HikeComponent } from './hike/hike.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HikeComponent
   ],
   imports: [
     BrowserModule
